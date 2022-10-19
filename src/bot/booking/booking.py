e20b3b9b5d4ca9e5410563f42d994bdeff7a030f
Shekhar-rv <shekhar.rv@gmail.com>
2022-10-19 20:28:12 +0100

    Added logic for searching for a deal WIP

--- src/bot/booking/booking.py
+++ src/bot/booking/booking.py
@@ -1,6 +1,7 @@
+from decouple import config
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from decouple import config
+from selenium.webdriver.common.by import By
 from booking import constants as const
 
 # Here the code will house a class with instances of the booking
@@ -18,6 +19,8 @@ class Booking():
         self.options.binary_location = self.browser_path
         self.chrome_driver_binary = Service(self.driver_path)
         self.driver = webdriver.Chrome(service=self.chrome_driver_binary, options=self.options)
+        self.driver.implicitly_wait(10)
+        self.driver.maximize_window()
 
     def __enter__(self):
         return self
@@ -28,3 +31,38 @@ class Booking():
 
     def land_first_page(self):
         self.driver.get(const.BASE_URL)
+
+    def change_currency(self, currency=None):
+        currency_element = self.driver.find_element(
+            By.CSS_SELECTOR, 
+            'button[data-tooltip-text="Choose your currency"]'
+        )
+        currency_element.click()
+        selected_currency_element = self.driver.find_element(
+            By.CSS_SELECTOR,
+            f'a[data-modal-header-async-url-param*="selected_currency={currency}"]'
+        )
+        selected_currency_element.click()
+
+    def select_destination(self, destination=None):
+        destination_element = self.driver.find_element(By.NAME, "ss")
+        destination_element.clear()
+        destination_element.send_keys(destination)
+        first_suggestion_element = self.driver.find_element(
+            By.CSS_SELECTOR,
+            'li[data-i="0"]'
+        )
+        first_suggestion_element.click()
+
+    def select_dates(self, check_in_date=None, check_out_date=None):
+        check_in_element = self.driver.find_element(
+            By.CSS_SELECTOR, 
+            f'td[data-date="{check_in_date}"]'
+        )
+        check_in_element.click()
+        
+        check_out_element = self.driver.find_element(
+            By.CSS_SELECTOR, 
+            f'td[data-date="{check_out_date}"]'
+        )
+        check_out_element.click()
--- src/bot/run.py
+++ src/bot/run.py
@@ -2,4 +2,6 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
-    print("Exiting...")
+    bot.change_currency(currency="USD")
+    bot.select_destination(destination="New York")
+    bot.select_dates(check_in_date="2022-11-01", check_out_date="2022-11-05")
